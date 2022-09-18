c41998dd83f4df1e93249014b6822a462d203404
John Lindgren <john@jlindgren.net>
2022-09-18 10:38:12 -0400

    common: Add znew/znew_n/znew_for() macros

--- include/common/mem.h
+++ include/common/mem.h
@@ -11,6 +11,24 @@
  */
 void *xzalloc(size_t size);
 
+/* Type-safe macros in the style of C++ new/new[] */
+#define znew(type)       ((type *)xzalloc(sizeof(type)))
+#define znew_n(type, n)  ((type *)xzalloc((n) * sizeof(type)))
+
+/*
+ * Variant of znew() that infers type from the passed pointer.
+ * Preferred over plain znew() in pointer declarations since it
+ * does not require repeating the type name.
+ *
+ * Example:
+ *   struct wlr_box *box = znew_for(box);
+ *
+ * Note:
+ *   In C23 "typeof" will be standard. Until then, __typeof__ works
+ *   with both GCC and clang (do we care about other compilers?)
+ */
+#define znew_for(ptr)  ((__typeof__(ptr))xzalloc(sizeof(*(ptr))))
+
 /*
  * As defined in FreeBSD.
  * Like realloc(), but calls exit() on error.
