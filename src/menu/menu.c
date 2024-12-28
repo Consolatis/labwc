70417108b504d3b4481a57cd2cfe45c9e13a6eb9
Consolatis <35009135+Consolatis@users.noreply.github.com>
2024-12-28 10:39:15 +0100

    [proof-of-concept] use async spawn for top-level-pipe-menu

--- src/menu/menu.c
+++ src/menu/menu.c
@@ -1,7 +1,6 @@
 // SPDX-License-Identifier: GPL-2.0-only
 #define _POSIX_C_SOURCE 200809L
 #include <assert.h>
-#include <glib.h>
 #include <libxml/parser.h>
 #include <libxml/tree.h>
 #include <signal.h>
@@ -49,6 +48,7 @@ static struct menuitem *selected_item;
 struct menu_pipe_context {
 	struct server *server;
 	struct menuitem *item;
+	struct menu *top_level_menu;
 	struct buf buf;
 	struct wl_event_source *event_read;
 	struct wl_event_source *event_timeout;
@@ -608,25 +608,32 @@ is_toplevel_static_menu_definition(xmlNode *n, char *id)
 }
 
 static bool parse_buf(struct server *server, struct buf *buf);
+static int handle_pipemenu_readable(int fd, uint32_t mask, void *_ctx);
+static int handle_pipemenu_timeout(void *_ctx);
 
 static void
-parse_root_pipemenu(struct server *server, const char *execute)
+parse_root_pipemenu(struct menu *top_level_menu, const char *execute)
 {
-	gchar *output;
-	GError *err = NULL;
-
-	if (!g_spawn_command_line_sync(execute, &output, NULL, NULL, &err)) {
-		wlr_log(WLR_ERROR, "failed to execute root pipe-menu '%s': %s",
-			execute, err->message);
-		g_error_free(err);
+	int pipe_fd = 0;
+	pid_t pid = spawn_piped(execute, &pipe_fd);
+	if (pid <= 0) {
+		wlr_log(WLR_ERROR, "Failed to spawn pipe menu process %s", execute);
 		return;
 	}
 
-	struct buf buf = BUF_INIT;
-	buf_add(&buf, output);
-	parse_buf(server, &buf);
-	g_free(output);
-	buf_reset(&buf);
+	struct menu_pipe_context *ctx = znew(*ctx);
+	ctx->server = top_level_menu->server;
+	ctx->top_level_menu = top_level_menu;
+	ctx->pid = pid;
+	ctx->pipe_fd = pipe_fd;
+	ctx->buf = BUF_INIT;
+
+	ctx->event_read = wl_event_loop_add_fd(ctx->server->wl_event_loop,
+		pipe_fd, WL_EVENT_READABLE, handle_pipemenu_readable, ctx);
+
+	ctx->event_timeout = wl_event_loop_add_timer(ctx->server->wl_event_loop,
+		handle_pipemenu_timeout, ctx);
+	wl_event_source_timer_update(ctx->event_timeout, PIPEMENU_TIMEOUT_IN_MS);
 }
 
 /*
@@ -653,10 +660,8 @@ handle_menu_element(xmlNode *n, struct server *server)
 			 *   <menu id="root-menu" label="foo" execute="bar"/>
 			 * </openbox_menu>
 			 */
-			++menu_level;
-			current_menu = menu_create(server, id, label);
-			parse_root_pipemenu(server, execute);
-			--menu_level;
+			struct menu *menu = menu_create(server, id, label);
+			parse_root_pipemenu(menu, execute);
 		} else {
 			current_item = item_create(current_menu, label,
 				/* arrow */ true);
@@ -1361,6 +1366,27 @@ menu_open_root(struct menu *menu, int x, int y)
 static void
 create_pipe_menu(struct menu_pipe_context *ctx)
 {
+	if (ctx->top_level_menu) {
+		if (ctx->server->menu_current == ctx->top_level_menu) {
+			menu_close_root(ctx->server);
+		}
+		struct menuitem *item, *tmp;
+		wl_list_for_each_safe(item, tmp, &ctx->top_level_menu->menuitems, link) {
+			item_destroy(item);
+		}
+		menu_level++;
+		current_menu = ctx->top_level_menu;
+		if (!parse_buf(ctx->server, &ctx->buf)) {
+			wlr_log(WLR_ERROR, "Failed to parse piped top level menu %s",
+				ctx->top_level_menu->id);
+		}
+		menu_level--;
+		post_processing(ctx->server);
+		validate(ctx->server);
+		menu_update_scene(current_menu);
+		return;
+	}
+
 	assert(ctx->item);
 
 	struct menu *pipe_parent = ctx->item->parent;
@@ -1449,7 +1475,7 @@ handle_pipemenu_readable(int fd, uint32_t mask, void *_ctx)
 	char data[8193];
 	ssize_t size;
 
-	if (!ctx->item) {
+	if (!ctx->item && !ctx->top_level_menu) {
 		/* parent menu item got destroyed in the meantime */
 		wlr_log(WLR_INFO, "[pipemenu %ld] parent menu item destroyed",
 			(long)ctx->pid);
@@ -1464,14 +1490,15 @@ handle_pipemenu_readable(int fd, uint32_t mask, void *_ctx)
 
 	if (size == -1) {
 		wlr_log_errno(WLR_ERROR, "[pipemenu %ld] failed to read data (%s)",
-			(long)ctx->pid, ctx->item->execute);
+			(long)ctx->pid, ctx->item ? ctx->item->execute : "n/a");
 		goto clean_up;
 	}
 
 	/* Limit pipemenu buffer to 1 MiB for safety */
 	if (ctx->buf.len + size > PIPEMENU_MAX_BUF_SIZE) {
 		wlr_log(WLR_ERROR, "[pipemenu %ld] too big (> %d bytes); killing %s",
-			(long)ctx->pid, PIPEMENU_MAX_BUF_SIZE, ctx->item->execute);
+			(long)ctx->pid, PIPEMENU_MAX_BUF_SIZE,
+			ctx->item ? ctx->item->execute : "n/a");
 		kill(ctx->pid, SIGTERM);
 		goto clean_up;
 	}
